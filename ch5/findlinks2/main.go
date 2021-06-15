05b2fba9e739fb21e6853b6ecb913031d9fb6500
duanbiaowu <codean.net@gmail.com>
2021-06-15 22:52:16 +0800

    [feat] add some code from Ch5

---
+++ ch5/findlinks2/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "findlin2: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s\n", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v\n", url, err)
+	}
+	return visit(nil, doc), nil
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	return visit(visit(links, n.FirstChild), n.NextSibling)
+}
